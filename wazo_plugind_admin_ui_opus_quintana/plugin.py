a30e1f582866f7a81bc6f7dad2d64c27c499ea15
Sylvain Boily <sylvainboilydroid@gmail.com>
2017-06-08 16:32:06 -0400

    add form

--- wazo_plugind_admin_ui_opus_quintana/plugin.py
+++ wazo_plugind_admin_ui_opus_quintana/plugin.py
@@ -2,14 +2,17 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+
+from flask import render_template
 from flask_menu.classy import register_flaskview
+from flask_menu.classy import classy_menu_item
 
 from wazo_admin_ui.helpers.plugin import create_blueprint
-from flask import render_template
+from wazo_admin_ui.helpers.classful import BaseView
+from wazo_admin_ui.helpers.form import BaseForm
 
-from flask_menu.classy import classy_menu_item
-from wazo_admin_ui.helpers.classful import LoginRequiredView
-
+from wtforms.fields import SubmitField, StringField, SelectField, BooleanField
+from wtforms.validators import InputRequired, Length
 
 opus = create_blueprint('opus', __name__)
 
@@ -19,15 +22,38 @@ class Plugin(object):
     def load(self, dependencies):
         core = dependencies['flask']
 
+        OpusConfigurationView.service = OpusService()
         OpusConfigurationView.register(opus, route_base='/opus_configuration')
         register_flaskview(opus, OpusConfigurationView)
 
         core.register_blueprint(opus)
 
 
-class OpusConfigurationView(LoginRequiredView):
+class OpusForm(BaseForm):
+    name = StringField('Name', [Length(max=128),])
+    packet_loss = StringField('Packet Loss', [Length(max=128),]) # 0 - 100
+    complexity = StringField('Complexity', [Length(max=128),]) # 0 - 10
+    signal = SelectField('Application', choices=[('auto', 'Auto'), ('voice', 'Voice'), ('music', 'Music')])
+    application = SelectField('Application', choices=[('voip', 'VOIP'), ('audio', 'Audio'), ('low_delay', 'Low Delay')])
+    max_playback_rate = StringField('Max Playback Rate', [Length(max=128),]) # 8000 - 48000
+    bitrate = StringField('Bite Rate', [Length(max=128),]) # 500 - 512000
+    cbr = BooleanField('CBR')
+    fec = BooleanField('FEC')
+    dtx = BooleanField('DTX')
+    submit = SubmitField('Submit')
+
+
+class OpusConfigurationView(BaseView):
+
+    form = OpusForm
+    resource = 'opus'
 
     @classy_menu_item('.advanced.opus', 'Opus', order=9, icon="heart")
     def index(self):
-        return render_template('opus/index.html')
+        return super(OpusConfigurationView, self).index()
 
+
+class OpusService(object):
+
+    def list(self):
+        return {'items': []}
