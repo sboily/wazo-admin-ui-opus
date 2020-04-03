e4e407ba1546a124fe7cf01514e4c8b58a46e0de
Sylvain Boily <sylvainboilydroid@gmail.com>
2020-04-03 13:49:12 -0400

    fix menuu

--- wazo_ui_opus_quintana/plugin.py
+++ wazo_ui_opus_quintana/plugin.py
@@ -9,12 +9,12 @@ import json
 
 from flask import render_template
 from flask_menu.classy import register_flaskview
-from flask_menu.classy import classy_menu_item
 from flask_babel import lazy_gettext as l_
 
 from wazo_ui.helpers.plugin import create_blueprint
 from wazo_ui.helpers.classful import BaseView
 from wazo_ui.helpers.form import BaseForm
+from wazo_ui.helpers.menu import menu_item
 
 from wtforms.fields import SubmitField, StringField, SelectField
 from wtforms.fields.html5 import IntegerField
@@ -57,7 +57,7 @@ class OpusConfigurationView(BaseView):
     form = OpusForm
     resource = 'opus'
 
-    @classy_menu_item('.ipbx.advanced.opus', l_('Opus'), icon="compress")
+    @menu_item('.ipbx.advanced.opus', l_('Opus'), icon="compress")
     def index(self):
         return super(OpusConfigurationView, self).index()
 
